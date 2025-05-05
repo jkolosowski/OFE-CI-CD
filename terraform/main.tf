1d1807bc6240522255c321c02a64ccd3c8d37be7
Jakub Kołosowski <kuba.kolosowski@icloud.com>
2025-05-05 09:49:35 +0200

    Add Lambda and API Gateway to monitor load time

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.40.0"
     }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.3"
+    }
   }
 
   required_version = ">= 1.5.0"
@@ -11,4 +16,14 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
+}
+
+module "perf" {
+  source = "./modules/perf"
+
+  lambda_function_name = "website_perf_check"
+
+  # Important - adjust lambda path to your project structure
+  lambda_source_dir = "${path.root}/../lib/lambda-perf"
+
 }
